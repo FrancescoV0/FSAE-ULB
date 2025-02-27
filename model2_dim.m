7fba97384acd892f65a9a1c334f210acdfa24a14
FrancescoV0 <112645278+FrancescoV0@users.noreply.github.com>
2025-02-27 19:39:53 +0100

    update

--- model2_dim.m
+++ model2_dim.m
@@ -6,4 +6,10 @@ tyre.offset = 0.1;
 tyre.stiffness = 1;
 tyre.damping = 1;
 
+%
+suspension = struct;
+suspension.front.radius = 0.02;
+suspension.front.length = 1;
 
+suspension.rear.radius = 0.02;
+suspension.rear.length = 1;
