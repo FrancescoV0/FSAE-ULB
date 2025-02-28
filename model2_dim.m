09ea0f677b609d8b861ee94e02b09866d9906cc1
FrancescoV0 <112645278+FrancescoV0@users.noreply.github.com>
2025-03-01 00:30:43 +0100

    updates

--- model2_dim.m
+++ model2_dim.m
@@ -1,6 +1,6 @@
 tyre = struct;
-tyre.radius = 0.5;
-tyre.width = 0.5;
+tyre.radius = 0.25;
+tyre.width = 0.3;
 tyre.offset = 0.1;
 
 tyre.stiffness = 1;
@@ -8,8 +8,12 @@ tyre.damping = 1;
 
 %
 suspension = struct;
-suspension.front.radius = 0.02;
-suspension.front.length = 1;
 
-suspension.rear.radius = 0.02;
-suspension.rear.length = 1;
+suspension.front.upper.radius = 0.02;
+suspension.front.upper.length = 1;
+suspension.front.lower.radius = 0.02;
+suspension.front.lower.length = 1;
+suspension.rear.upper.radius = 0.02;
+suspension.rear.upper.length = 1;
+suspension.rear.lower.radius = 0.02;
+suspension.rear.lower.length = 1;
