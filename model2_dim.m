60a538f664d0967d011e2767ef19d5e3da966388
FrancescoV0 <112645278+FrancescoV0@users.noreply.github.com>
2025-03-01 16:22:24 +0100

    Rear suspension geometry

--- model2_dim.m
+++ model2_dim.m
@@ -1,19 +1,35 @@
+%% Tyres
+
 tyre = struct;
-tyre.radius = 0.25;
-tyre.width = 0.3;
-tyre.offset = 0.1;
+tyre.radius = 25.5; %cm
+tyre.width = 20.5; %cm
 
 tyre.stiffness = 1;
 tyre.damping = 1;
 
-%
+%% Suspensions
 suspension = struct;
 
-suspension.front.upper.radius = 0.02;
+h1 = 15; %cm
+hub_offset = 2.5; %cm
+d = 30; %cm
+
+%front suspensions
+suspension.front.upper.radius = 2; %cm
 suspension.front.upper.length = 1;
-suspension.front.lower.radius = 0.02;
+
+suspension.front.lower.radius = 2; %cm
 suspension.front.lower.length = 1;
-suspension.rear.upper.radius = 0.02;
-suspension.rear.upper.length = 1;
-suspension.rear.lower.radius = 0.02;
-suspension.rear.lower.length = 1;
+
+
+%rear suspension
+suspension.rear.upper.radius = 2; %cm
+suspension.rear.upper.length = sqrt((d)^2+(h1 + 20.5 - tyre.radius - hub_offset)^2); %cm
+suspension.rear.upper.inclination = atan((h1 + 20.5 - tyre.radius - hub_offset)/d) * 180/pi; %°
+
+suspension.rear.lower.radius = 2; %cm
+suspension.rear.lower.length = sqrt((d)^2+(h1 - tyre.radius + hub_offset)^2); %cm
+suspension.rear.lower.inclination = atan((h1 - tyre.radius + hub_offset)/d) * 180/pi; %°
+
+
+ 
