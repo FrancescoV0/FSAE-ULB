e59f49842eb1bab157954a1aacbdf4cfbd1da29e
simulatorATM <simulatorATM@DESKTOP-S1J7HP7>
2025-05-07 16:45:48 +0200

    plot in separated file qnd G29 implementation for real time

--- model2_dim.m
+++ model2_dim.m
@@ -124,47 +124,9 @@ Aero.Drag_reduced = 0.837;
 g = 9.81;
 slope_angle = 0;
 
-%% Logs out:
-Table = out.logsout.extractTimetable;
-out.SimulationMetadata.TimingInfo
-writetimetable(Table, 'logsout.xlsx');
-%% Plots
-close all;
-plot(Table.Time, Table.("chassis speed (chassis ref)"))
-
-%Cornering forces
-figure();
-plot(Table.Time, Table.Fy_FL, Table.Time, Table.Fy_FR, Table.Time, Table.Fy_RL, Table.Time, Table.Fy_RR, 'LineWidth', 1);
-title('Cornering forces')
-xlabel('time [s]');
-ylabel('Force [N]');
-legend({'FL', 'FR', 'RL', 'RR'});
-grid();
-
-% Aerodynamic forces
-figure();
-plot(Table.Time, Table.Aero_downforce, Table.Time, Table.Aero_drag, Table.Time, Table.Aero_lateralforce, 'LineWidth', 1);
-title('Aerodynamic forces')
-xlabel('time [s]');
-ylabel('Force [N]');
-legend({'Down focre', 'Drag', 'Lat'});
-grid();
-
-% Slip angles
-figure();
-plot(Table.Time, Table.("Effective slip angle_FL"), Table.Time, Table.("Effective slip angle_FR"), Table.Time, Table.("Effective slip angle_RL"), Table.Time, Table.("Effective slip angle_RR"), 'LineWidth', 1)
-title('Slip angles')
-xlabel('time [s]');
-ylabel('SA [°]');
-legend({'FL', 'FR', 'RL', 'RR'});
-grid();
-
-% Position (time implicit)
-figure();
-plot(Table.pos(:,1), Table.pos(:,2),'LineWidth', 1)
-hold on
-plot(Table.pos(1,1), Table.pos(1,2), 'o') % Origin
-title('Trajectory')
-xlabel('x [m]');
-ylabel('y [m]');
-grid();
+%% G29
+G29 = struct;
+G29.steering.min = 0;
+G29.steering.max = 6.554e4;
+G29.pedal.min = 0; % ! fully pressed 
+G29.pedal.max = 255; 
---
+++ model2_plot.m
@@ -0,0 +1,51 @@
+%% Logs out:
+Table = out.logsout.extractTimetable;
+out.SimulationMetadata.TimingInfo
+writetimetable(Table, 'logsout.xlsx');
+%% Plots
+close all;
+figure()
+plot(Table.Time, Table.("chassis speed (chassis ref)"), 'LineWidth', 1)
+title('Chassis speed (chassis ref)')
+xlabel('time [s]');
+ylabel('Speed [m/s]');
+legend({'xdot', 'ydot', 'zdot'});
+grid();
+
+%Cornering forces
+figure();
+plot(Table.Time, Table.Fy_FL, Table.Time, Table.Fy_FR, Table.Time, Table.Fy_RL, Table.Time, Table.Fy_RR, 'LineWidth', 1);
+title('Cornering forces')
+xlabel('time [s]');
+ylabel('Force [N]');
+legend({'FL', 'FR', 'RL', 'RR'});
+grid();
+
+% Aerodynamic forces
+figure();
+plot(Table.Time, Table.Aero_downforce, Table.Time, Table.Aero_drag, Table.Time, Table.Aero_lateralforce, 'LineWidth', 1);
+title('Aerodynamic forces')
+xlabel('time [s]');
+ylabel('Force [N]');
+legend({'Down focre', 'Drag', 'Lat'});
+grid();
+
+% Slip angles
+figure();
+plot(Table.Time, Table.("Effective slip angle_FL"), Table.Time, Table.("Effective slip angle_FR"), Table.Time, Table.("Effective slip angle_RL"), Table.Time, Table.("Effective slip angle_RR"), 'LineWidth', 1)
+title('Slip angles')
+xlabel('time [s]');
+ylabel('SA [°]');
+legend({'FL', 'FR', 'RL', 'RR'});
+grid();
+
+% Position (time implicit)
+figure();
+plot(Table.pos(:,1), Table.pos(:,2),'LineWidth', 1)
+hold on
+plot(Table.pos(1,1), Table.pos(1,2), 'o') % Origin
+title('Trajectory')
+xlabel('x [m]');
+ylabel('y [m]');
+grid();
+
